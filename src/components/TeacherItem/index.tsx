88c85542a453fd2dab5406de8711e51dda1d733f
Luan Ogawa <ogawa.luan@gmail.com>
2020-08-06 03:07:00 -0300

    day 3

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,35 +3,51 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: number;
+    
+}
+
+interface TeacherItemProps {
+    teacher:  Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('/connections', {
+            user_id: teacher.id,
+        })
+    }
+    
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/62737540?s=460&u=09e9ee790a8aa4fddae19edd23e6c2c604390eae&v=4" alt="Luan Ogawa"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Luan Ogawa</strong>
-                    <span>Lógica</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
             
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                <br/>
-                Numquam quaerat similique necessitatibus velit nostrum, delectus deserunt qui laborum et sint eius fuga sit consectetur cum provident id saepe doloremque! 
-                <br/>
-                Possimus!
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/Hora
-                    <strong>R$50,00</strong>
+                    <strong>R${teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
--- src/pages/TeachersList/index.tsx
+++ src/pages/TeachersList/index.tsx
@@ -1,38 +1,86 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
-import PageHeader from '../../components/PageHeader';
 import './styles.css';
-import TeacherItem from '../../components/TeacherItem';
+import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import api from '../../services/api';
 
 function TeacherList() {
+    const [teachers, setTeachers] = useState([])
+    
+    const [subject, setSubject] = useState('')
+    const [week_day, setWeekDay] = useState('')
+    const [time, setTime] = useState('')
+
+    async function searchTeachers (e: FormEvent) {
+        e.preventDefault()
+
+        const response = await api.get('/classes', {
+            params: {
+                subject,
+                week_day,
+                time,
+            }
+        })
+
+        setTeachers(response.data)
+    }
+    
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os Proffys disponíveis"> 
-                <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matérias</label>
-                        <input type="text" id="subject"/>
-                    </div>
+                <form id="search-teachers" onSubmit={searchTeachers}>
+                    <Select 
+                        name="subject" 
+                        label="Matéria"
+                        value={subject}
+                        onChange={(e) => { setSubject(e.target.value) }}
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Ciências', label: 'Ciências' },
+                            { value: 'Ed. Física', label: 'Ed. Física' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Química', label: 'Química' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Inglês', label: 'Inglês' },
+                            { value: 'Espanhol', label: 'Espanhol' },
+                            { value: 'Lógica', label: 'Lógica' },
+                            { value: 'Literatura', label: 'Literatura' },
+                            { value: 'Sociologia', label: 'Sociologia' },
+                            { value: 'Filosofia', label: 'Filosofia' },
+                            { value: 'Geografia', label: 'Geografia' },
+                    ]}
+                    />
 
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da Semana</label>
-                        <input type="text" id="subject"/>
-                    </div>
+                    <Select 
+                        name="week_day" 
+                        label="Dia da Semana"
+                        value={week_day}
+                        onChange={(e) => { setWeekDay(e.target.value) }}
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-feira' },
+                            { value: '2', label: 'Terça-feira' },
+                            { value: '3', label: 'Quarta-feira' },
+                            { value: '4', label: 'Quinta-feira' },
+                            { value: '5', label: 'Sexta-feira' },
+                            { value: '6', label: 'Sábado' },
+                    ]}
+                    /> 
 
-                    <div className="input-block">
-                        <label htmlFor="time">Horário</label>
-                        <input type="text" id="subject"/>
-                    </div>
+                    <Input type="time" name="time" label="Horário" value={time} onChange={(e) => { setTime(e.target.value) }} />
+                    <button type='submit'>Buscar</button>
                 </form>
             </PageHeader>
 
             <main>
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                {teachers.map((teacher: Teacher) => {
+                    return <TeacherItem key={teacher.id} teacher={teacher} />
+                })}
             </main>
         </div>
     )
